78499079786913753f4f3ab8663ad50d3585bdb6
Christopher DeMarchi <Christopher.k.demarchi@gmail.com>
2021-06-03 21:02:36 -0400

    Added in the graphing function Swapped out the problem specific plugins to a more general solution

--- Summation_Function.py
+++ Summation_Function.py
@@ -19,9 +19,16 @@ class Series():
     self.ao = ao
     self.an = an
     self.bn = bn
+
     self.series = ao + an * math.cos(n * x) + bn * math.sin(n * x)
 
-def graph(self):
+def graph(self, Length = pi, Harmonic):
+    x = np.linspace(-Length, Length, 50) #Creates the domain to graph the function over 
+    ones_mat = np.ones(x.size, dtype = float) #initializes an array of 1s to fill later in the loop
+    function = self.ao * ones_mat
+
+    for n in range(1,Harmonic,1):
+        function = self.ao + self.an * np.cos(n * x) + self.bn * np.sin(n * x) #Combines all the constants with the general Fourier series
 
 def Summation_Function(ao,an,bn):
     """ Takes constants of the Fourier Transform and makes them into a summation
