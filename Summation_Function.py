fe965048080e10a72698cdbc2e7898839a04363b
Christopher DeMarchi <Christopher.k.demarchi@gmail.com>
2021-06-04 11:10:28 -0400

    Fixed Syntax errors and deleted selfseries attribu

--- Summation_Function.py
+++ Summation_Function.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sympy as sy
 import array as arr
-
 class Series(): 
     """Series parent class
     Attributes - 
@@ -13,23 +12,15 @@ class Series():
     Methods - 
         Graph
     """
-    __init__(self, ao, an, bn): 
-    """ Takes in ao, an, bn in terms of n and x """
+    def __init__(self, ao, an, bn):
+        self.ao = ao
+        self.an = an
+        self.bn = bn
 
-    self.ao = ao
-    self.an = an
-    self.bn = bn
+    def graph(self, Length = pi, Harmonic = 1):
+        x = np.linspace(-Length, Length, 50) #Creates the domain to graph the function over 
+        ones_mat = np.ones(x.size, dtype = float) #initializes an array of 1s to fill later in the loop
+        series = self.ao * ones_mat
 
-    self.series = ao + an * math.cos(n * x) + bn * math.sin(n * x)
-
-def graph(self, Length = pi, Harmonic):
-    x = np.linspace(-Length, Length, 50) #Creates the domain to graph the function over 
-    ones_mat = np.ones(x.size, dtype = float) #initializes an array of 1s to fill later in the loop
-    function = self.ao * ones_mat
-
-    for n in range(1,Harmonic,1):
-        function = self.ao + self.an * np.cos(n * x) + self.bn * np.sin(n * x) #Combines all the constants with the general Fourier series
-
-def Summation_Function(ao,an,bn):
-    """ Takes constants of the Fourier Transform and makes them into a summation
-    """
+        for n in range(1,Harmonic,1):
+            series = self.ao + self.an * np.cos(n * x) + self.bn * np.sin(n * x) #Combines all the constants with the general Fourier series
