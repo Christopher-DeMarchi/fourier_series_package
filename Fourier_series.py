04fa76615a25abd89a194e52da6be7cfe03f32e5
Christopher DeMarchi <Christopher.k.demarchi@gmail.com>
2021-06-08 15:38:27 -0400

    first working code

--- Fourier_series.py
+++ Fourier_series.py
@@ -8,30 +8,34 @@ from scipy import integrate
 
 #passing a function to a class 
 
-L_1 = -pi
-L_2 = pi
+L_1 = -1
+L_2 = 1
 
 def f(x):
-    return x**2
+    return x
+
 
 def fcos(x,n,L_2):
-    return f(x) * cos(n * pi * x / L_2)
+    return f(x) * np.cos(n * pi * x / L_2)
 
 def fsin(x,n,L_2):
-    return f(x) * sin(n * pi * x / L_2)
+    return f(x) * np.sin(n * pi * x / L_2)
 
-#f = x**2
 
-y = np.linspace(-pi, pi, 50)
-ao = np.ones(y.size, dtype = float)
-a_0 = (1 / L_2) * integrate.quad(f, L_1, L_2)[0]
+domain = np.linspace(-pi, pi, 50)
+ao = np.ones(domain.size, dtype = float)
+
+a_0 = (1 / (2*L_2)) * integrate.quad(f, L_1, L_2)[0]
+
+print(a_0)
 
 #a_n = (1 / L_2) * integrate.quad(fcos, L_1, L_2, args=(n, L_2))[0]
+
 #b_n = (1 / L_2) * integrate.quad(fsin, L_1, L_2, args=(n, L_2))[0]
 
 ans = ao * a_0 
 print(len(ans))
-print(len(y))
+print(len(domain))
 
 def a_n(n,L_1,L_2):
     return (1 / L_2) * integrate.quad(fcos, L_1, L_2, args=(n, L_2))[0]
@@ -40,8 +44,14 @@ def b_n(n,L_1,L_2):
     return (1 / L_2) * integrate.quad(fsin, L_1, L_2, args=(n, L_2))[0]
 
 for n in range(1, 25, 1):
-    ans = ans + (a_0 / 2) + (a_n(n,L_1,L_2) * cos(n * pi * y / L_2)) + (b_n(n,L_1,L_2) * sin(n * pi * y / L_2))
-plt.plot(y,ans)
+    a = a_n(n,L_1,L_2)
+    b = b_n(n,L_1,L_2)
+    ans = ans + (a * np.cos(n * pi * domain / L_2)) + (b * np.sin(n * pi * domain / L_2))
+
+#for n in range (1,3):
+#    print(np.cos(n*pi*domain/L_2))
+
+plt.plot(domain,ans)
 plt.xlabel('x-axis')
 plt.ylabel('y-axis')
 plt.title('yay')
